a0a9da7bd1c6a702948a603b2113a53382c69e9a
salehfaraj08 <89396137+salehfaraj08@users.noreply.github.com>
2021-11-20 23:31:44 +0200

    fianl edetion

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/getCustomer/:passportid', async (req, res) => {
             throw err;
         if (data && data.length > 0)
             return res.status(200).send(data);
-        return res.status(404).send({ error: 'there is no book published on this year' });
+        return res.status(404).send({ error: 'these customer is not exist' });
     });
 });
 
@@ -32,59 +32,48 @@ app.post('/', async (req, res) => {
         cash,
         credit
     });
+    const data = await bankModel.find({});
+    const customer = data.find(customer => customer.passportId === passportId);
+    console.log(customer);
+    if (customer) {
+        return res.status(400).json({ error: 'Customer is already exist' });
+    }
     user.save((err, data) => {
-        if (err) return res.status(404).send(err.message);
+        if (err) return res.status(404).send(err);
         return res.status(200).send(data);
     });
 });
 
 app.put('/bank/deposit/:id', async (req, res) => {
     const { id } = req.params;
-    const { amountOfCash } = parseInt(req.body.amountOfCash);
-    const user = {
-        
-    };
-    bookModel.findByIdAndUpdate(updateId, book, { new: true, runValidators: true }, (err, data) => {
-        if (err) return res.status(404).send(err.message);
-        return res.status(200).send(data);
-    });
-
-
-
-    let customer = file.customers.find(customer => customer.id === parseInt(id));
-    if (!customer) {
-        return res.status(400).json({ error: 'customer is not exist' })
-    }
+    const amountOfCash = parseInt(req.body.amountOfCash);
+    console.log('amount', amountOfCash);
     if (amountOfCash > 0) {
-        customer.cash += amountOfCash
-        fs.writeFile('Customers.json', JSON.stringify(file), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        })
-        return res.status(200).json({ success: 'cash updated successfully' });
+        bankModel.findById(id, (err, user) => {
+            if (err) return res.status(404).send(err);
+            bankModel.findByIdAndUpdate(id, { cash: user.cash + amountOfCash }, { new: true, runValidators: true }, (err, data) => {
+                if (err) return res.status(404).send(err);
+                return res.status(200).send(user);
+            });
+        });
+
     }
     else {
-        return res.status(400).json({ error: 'cannot deposit a negative number' })
+        return res.status(400).json({ error: 'cannot update a negative number to credit' })
     }
 });
 
 app.put('/bank/updateCredit/:id', async (req, res) => {
     const { id } = req.params;
     const newCredit = parseInt(req.body.newCredit);
-    let file = await getAllCustomers();
-    let customer = file.customers.find(customer => customer.id === parseInt(id));
-    if (!customer) {
-        return res.status(400).json({ error: 'customer is not exist' })
-    }
     if (newCredit > 0) {
-        customer.credit += newCredit
-        fs.writeFile('Customers.json', JSON.stringify(file), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        })
-        return res.status(200).json({ success: 'credit updated successfully' });
+        bankModel.findById(id, (err, user) => {
+            if (err) return res.status(404).send(err);
+            bankModel.findByIdAndUpdate(id, { credit: user.credit + newCredit }, { new: true, runValidators: true }, (err, data) => {
+                if (err) return res.status(404).send(err);
+                return res.status(200).send(user);
+            });
+        });
     }
     else {
         return res.status(400).json({ error: 'cannot update a negative number to credit' })
@@ -94,76 +83,69 @@ app.put('/bank/updateCredit/:id', async (req, res) => {
 app.put('/bank/withdraw/:id', async (req, res) => {
     const { id } = req.params;
     const withdrawAmount = parseInt(req.body.withdrawAmount);
-    let file = await getAllCustomers();
-    let customer = file.customers.find(customer => customer.id === parseInt(id));
-    if (!customer) {
-        return res.status(400).json({ error: 'customer is not exist' })
-    }
-    if (withdrawAmount <= customer.cash && withdrawAmount > 0) {
-        customer.cash -= withdrawAmount;
-        fs.writeFile('Customers.json', JSON.stringify(file), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        })
-        return res.status(200).json({ success: 'withdraw money successfully' });
-    }
-    else if (customer.cash + customer.credit >= withdrawAmount && withdrawAmount > 0) {
-        customer.cash -= withdrawAmount;
-        fs.writeFile('Customers.json', JSON.stringify(file), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        })
-        return res.status(200).json({ success: 'withdraw money successfully' });
-    }
-    else {
-        return res.status(400).json({ error: 'cannot withdraw there is no enough money/credit' })
-    }
+    bankModel.findById(id, (err, user) => {
+        if (err) return res.status(404).send(err);
+        if (withdrawAmount <= user.cash && withdrawAmount > 0) {
+            bankModel.findByIdAndUpdate(id, { cash: user.cash - withdrawAmount }, { new: true, runValidators: true }, (err, data) => {
+                if (err) return res.status(404).send(err);
+                return res.status(200).send(user);
+            });
+        }
+        else if (user.cash + user.credit >= withdrawAmount && withdrawAmount > 0) {
+            bankModel.findByIdAndUpdate(id, { cash: user.cash - withdrawAmount }, { new: true, runValidators: true }, (err, data) => {
+                if (err) return res.status(404).send(err);
+                return res.status(200).send(user);
+            });
+        }
+        else {
+            return res.status(400).json({ error: 'cannot withdraw there is no enough money/credit' })
+        }
+    });
 });
 
 ////tranfer from customer to another
 app.put('/bank/transfer/:id/:recieverId', async (req, res) => {
     const { id, recieverId } = req.params;
     const transferAmount = parseInt(req.body.transferAmount);
-    let file = await getAllCustomers();
-    let customer = file.customers.find(customer => customer.id === parseInt(id));
-    let reciever = file.customers.find(customer => customer.id === parseInt(recieverId));
-    if (!customer || !reciever) {
-        return res.status(400).json({ error: 'customer is not exist' })
-    }
-    if(customer.passportId===reciever.passportId){
-        return res.status(400).json({ error: 'cannot transfer from customer to the same customer' })
-    }
-    if (transferAmount <= customer.cash && transferAmount > 0) {
-        customer.cash -= transferAmount;
-        reciever.cash += transferAmount;
-        fs.writeFile('Customers.json', JSON.stringify(file), (err) => {
-            if (err) {
-                console.log(err);
+    console.log(id,recieverId,transferAmount);
+    bankModel.findById(id, (err, customer) => {
+        if (err) return res.status(404).send(err);
+        bankModel.findById(recieverId, (err, reciever) => {
+            if (err) return res.status(404).send(err);
+            if (customer.passportId === reciever.passportId) {
+                return res.status(400).json({ error: 'cannot transfer from customer to the same customer' })
             }
-        })
-        return res.status(200).json({ success: 'tranfered money successfully' });
-    }
-    else if (customer.cash + customer.credit >= transferAmount && transferAmount > 0) {
-        customer.cash -= transferAmount;
-        reciever.cash += transferAmount;
-        fs.writeFile('Customers.json', JSON.stringify(file), (err) => {
-            if (err) {
-                console.log(err);
+            if (transferAmount <= customer.cash && transferAmount > 0) {
+                bankModel.findByIdAndUpdate(id, { cash: customer.cash - transferAmount }, { new: true, runValidators: true }, (err, data) => {
+                    if (err) return res.status(404).send(err);
+                    bankModel.findByIdAndUpdate(recieverId, { cash: reciever.cash + transferAmount }, { new: true, runValidators: true }, (err, data) => {
+                        if (err) return res.status(404).send(err);
+                    });
+                    return res.status(200).send(customer);
+                });
+                
             }
-        })
-        return res.status(200).json({ success: 'tranfered money successfully' });
-    }
-    else {
-        return res.status(400).json({ error: 'cannot tranfer there is no enough money/credit' })
-    }
+            else if (customer.cash + customer.credit >= transferAmount && transferAmount > 0) {
+                bankModel.findByIdAndUpdate(id, { cash: customer.cash - transferAmount }, { new: true, runValidators: true }, (err, data) => {
+                    if (err) return res.status(404).send(err);
+                    bankModel.findByIdAndUpdate(recieverId, { cash: reciever.cash + transferAmount }, { new: true, runValidators: true }, (err, data) => {
+                        if (err) return res.status(404).send(err);
+                    });
+                    return res.status(200).send(customer);
+                });
+                
+            }
+            else {
+                return res.status(400).json({ error: 'cannot tranfer there is no enough money/credit' })
+            }
+        });
+    });
 });
 
-mongoose.connect('mongodb+srv://saleh:saleh0811@cluster0.whijz.mongodb.net/bankDb?retryWrites=true&w=majority', { useNewUrlParser: true },()=>{
+mongoose.connect('mongodb+srv://saleh:saleh0811@cluster0.whijz.mongodb.net/bankDb?retryWrites=true&w=majority', { useNewUrlParser: true }, () => {
     console.log('connected to db');
 });
 
-app.listen(process.env.PORT || 5001,()=>{
+app.listen(process.env.PORT || 5001, () => {
     console.log('listening on server 5001 or env');
 });
